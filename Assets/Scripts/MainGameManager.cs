b113f8525ca1f079b4a9e7e2a0e22b655d215d42
Maksym <eldiablo@ukr.net>
2021-11-29 17:00:05 +0200

    made buttons work

--- Assets/Scripts/MainGameManager.cs
+++ Assets/Scripts/MainGameManager.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.IO;
 
 public class MainGameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static MainGameManager instance;
+
+    public TextMeshProUGUI currentPlayer;
+
+    public string playerName;
+    public InputField nameInput;
+    private void Awake()
     {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayerNameInput()
     {
-        
+        playerName = nameInput.text;
+        currentPlayer.text = "Player: " + playerName;
+        Debug.Log("name- " + playerName);
     }
+
+    /*[System.Serializable]
+    class SaveData
+    {
+        public string lastPlayerName;
+        public string lastScore;
+    }
+
+    public void SaveNameScore()
+    {
+        SaveData data = new SaveData();
+        data.lastPlayerName = playerName;
+        data.lastScore =
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            teamColor = data.teamColor;
+        }
+    }
+    */
 }
